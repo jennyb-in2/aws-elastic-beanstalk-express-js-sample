55beefa1399f0372131912799901ad87cd3ab8f5
Jennyb In2 <jennyb@intonowhere.com>
2021-09-17 09:53:47 -0600

    Testing full AWS pipline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Amaazing how much new stuff AWS provides developers!'));
+app.get('/', (req, res) => res.send('Is this easier than terraform?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
